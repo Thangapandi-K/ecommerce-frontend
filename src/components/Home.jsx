055f14794dee1023f024e21848f8d37c66560718
Thangapandi-K <thangapandi.chella@gmail.com>
2024-07-09 11:34:10 +0530

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header">
+                            E-Commerce
+                        </div>
+                        <div className="card-body">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
